be47ac331eeadf9fc2abc7baafc3d5626b3d9d2b
Stefano Montesi <dincio.montesi@gmail.com>
2020-03-17 15:53:01 +0100

    Complete implementation of config loading

--- src/phrases/mod.rs
+++ src/phrases/mod.rs
@@ -1,28 +1,61 @@
 use crate::world::*;
-use std::collections::HashMap;
+use lazy_static::*;
+use regex::Regex;
 use serde::Deserialize;
 
 /// All the prhases a judge might use to judge a zuppa.
-#[derive(Default, Deserialize)]
+#[derive(Default, Debug, PartialEq, Deserialize)]
 pub struct Phrases {
-    phrases: HashMap<Score, Phrase>,
+    phrases: Vec<(Score, String)>,
 }
 
 impl Phrases {
     /// Parse a special format string into a `Phrases` struct.
     pub fn parse(from: &str) -> Self {
-        unimplemented!()
+        // Regex to match single phrase.
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r#"(\d*):\s*([\.\-!'",;\[\]\(\){}\sa-zA-Z0-9_]*)$"#).unwrap();
+        }
+
+        Phrases {
+            phrases: from
+                .lines()
+                .map(|phrase| {
+                    let caps = RE.captures(phrase).unwrap_or_else(|| panic!("Malformed phrase: {}", phrase));
+
+                    // Info to construct phrase is extracted from captures of regex match.
+                    let score_upper_bound_exclusive = caps
+                        .get(1)
+                        .expect("Invalid upper range match in phrase config file")
+                        .as_str()
+                        .parse()
+                        .expect("Malformed phrase upper score range specifier");
+                    let phrase_template = caps
+                        .get(2)
+                        .expect("Invalid phrase template match in phrase config file")
+                        .as_str()
+                        .into();
+
+                    (score_upper_bound_exclusive, phrase_template)
+                })
+                .collect(),
+        }
     }
 }
 
-/// Single judge phrase.
-#[derive(Deserialize)]
-pub struct Phrase {
-    contents: String,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-impl Phrase {
-    pub fn gen(&self, world: &World, cook: CookKey, judge: JudgeKey) -> String {
-        unimplemented!()
+    #[test]
+    fn phrases_parse_single_phrase() {
+        assert_eq!(
+            Phrases::parse("0: I like this"),
+            Phrases {
+                phrases: vec! {
+                    (0, "I like this".into()),
+                }
+            }
+        )
     }
 }
